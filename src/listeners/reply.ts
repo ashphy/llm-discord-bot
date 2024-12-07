d872c1633f0608d0e4cbeb7a84f00b73f346ff9e
Kazuki Hamasaki <ne.vivam.si.abis@gmail.com>
2024-12-07 17:17:49 +0900

    bot自身への返信かどうかを確認するstepを追加

--- src/listeners/reply.ts
+++ src/listeners/reply.ts
@@ -16,32 +16,29 @@ export class MessageReplyListener extends Listener {
 	// AIに関する返信であれば、会話を継続する
 	public async run(message: Message) {
 		// ボットが送信したメッセージを無視
-		if (message.author.bot) {
-			return;
-		}
+		if (message.author.bot) return;
 
 		// 返信かどうか確認
-		if (message.reference?.messageId) {
-			const repliedMessage = message.channel.messages.cache.get(
-				message.reference.messageId,
-			);
+		if (!message.reference?.messageId) return;
+		const repliedMessage = message.channel.messages.cache.get(
+			message.reference.messageId,
+		);
+		if (!repliedMessage) return;
 
-			if (!repliedMessage) {
-				return;
-			}
+		// このBotへの返信か確認
+		if (this.container.client.user?.id !== repliedMessage.author.id) return;
 
-			// Botへの返信なので、会話を継続する
-			const referenceMessageId = message.reference.messageId;
-			const userName = repliedMessage.author.username;
-			const userMessage = message.content;
+		// Botへの返信なので、会話を継続する
+		const referenceMessageId = message.reference.messageId;
+		const userName = repliedMessage.author.username;
+		const userMessage = message.content;
 
-			// ここで返信メッセージに対して反応する処理を記述
-			const aiAgent = new AiAgent();
-			await aiAgent.load(referenceMessageId);
-			const answer = await aiAgent.thinkAnswer(userName, userMessage);
+		// ここで返信メッセージに対して反応する処理を記述
+		const aiAgent = new AiAgent();
+		await aiAgent.load(referenceMessageId);
+		const answer = await aiAgent.thinkAnswer(userName, userMessage);
 
-			const answerMessage = await message.reply(answer);
-			await aiAgent.save(answerMessage.id);
-		}
+		const answerMessage = await message.reply(answer);
+		await aiAgent.save(answerMessage.id);
 	}
 }
